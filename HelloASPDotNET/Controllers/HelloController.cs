3ee6fecb402b41ee235f71c75bfc348bc5a10170
JustynBougeno <93269509+JustynBougeno@users.noreply.github.com>
2021-12-13 11:01:26 -0600

    created MVCs

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -11,8 +11,17 @@ namespace HelloASPDotNET.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
             return View();
         }
     }
